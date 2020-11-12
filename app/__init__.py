46b449f4b5e10ba91d31820560d6fb0f6576ad0a
shkim4738 <shkim4738@gmail.com>
2020-11-12 12:57:38 +0900

    기본 파일 작성

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# 추가할 모듈이 있다면 추가
+# config 파일이 있다면 추가
+
+# 앞으로 새로운 폴더를 만들어서 파일을 추가할 예정임
+# from app.main.[파일이름] --> app/main 디렉토리 하위의 [파일 이름].py 를 import 한 것임
+from app.main.index import main as main
+
+# 위에서 추가한 파일을 연동해주는 역할
+app.register_blueprint(main)
