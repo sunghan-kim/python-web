682b19069bd5307f5b007e19835e0cc6809a6e3f
shkim4738 <shkim4738@gmail.com>
2020-11-16 20:33:17 +0900

    MySQL 연동하기 (DB 클래스 생성)

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 # 앞으로 새로운 폴더를 만들어서 파일을 추가할 예정임
 # from app.main.[파일이름] --> app/main 디렉토리 하위의 [파일 이름].py 를 import 한 것임
 from app.main.index import main as main
+from app.test.test import test as test
 
 # 위에서 추가한 파일을 연동해주는 역할
 app.register_blueprint(main)
+app.register_blueprint(test)
---
+++ app/schema/test.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE testDB default CHARACTER SET UTF8;
+
+use testDB;
+
+CREATE TABLE testTable (
+    idx     INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    test    VARCHAR(256) NOT NULL
+) CHARSET=utf8;
+
+/*
+
+ */
